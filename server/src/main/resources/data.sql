2e6c34d3f90d3e1b1e3f30adf6ab3a98a8ab29fb
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-13 13:54:04 -0300

    Testing connection to database

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
 	id_order VARCHAR(36) NOT NULL PRIMARY KEY,
 	id_client INT NOT NULL,
 	id_parcel INT NOT NULL,
@@ -8,3 +7,11 @@ CREATE TABLE orders (
 );
 -- INSERT INTO orders (id_client, id_parcel, total_value, order_address)
 -- VALUES (123, 456, 1.99, 'Rua de cima');
+
+CREATE TABLE IF NOT EXISTS ordersTest (
+	id_order VARCHAR(36) NOT NULL PRIMARY KEY,
+	id_client INT NOT NULL,
+	id_parcel INT NOT NULL,
+	total_value FLOAT NOT NULL,
+	order_address VARCHAR(1000) NOT NULL
+);
--- server/src/test/java/com/rufino/server/ServerApplicationTests.java
+++ server/src/test/java/com/rufino/server/ServerApplicationTests.java
@@ -1,13 +1,38 @@
 package com.rufino.server;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.rufino.server.Database.DatabaseConnection;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
 
+	private static Connection conn;
+
+	@BeforeAll
+	public static void openConnection() throws SQLException {
+		conn = DatabaseConnection.getInstance().getConnection();
+		assertNotNull(conn);
+	}
+
+	@AfterAll
+	public static void closeConnection() throws SQLException {
+		assertNotNull(conn);
+		conn.close();
+		assertEquals(true, conn.isClosed());
+	}
+
 	@Test
-	void contextLoads() {
+	void testInsert() {
+
 	}
 
 }
