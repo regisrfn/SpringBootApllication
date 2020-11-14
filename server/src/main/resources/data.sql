913e201fc6607f82129c5c8ec6d7400eaf50b898
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-14 13:21:53 -0300

    Creating empty orders table in the start

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS orders (
 	total_value FLOAT NOT NULL,
 	order_address VARCHAR(256) NOT NULL
 );
-INSERT INTO orders (id_order, id_client, id_parcel, total_value, order_address)
-VALUES (1, 123, 456, 1.99, 'Rua de cima');
+-- INSERT INTO orders (id_order, id_client, id_parcel, total_value, order_address)
+-- VALUES (1, 123, 456, 1.99, 'Rua de cima');
 
-CREATE TABLE IF NOT EXISTS ordersTest (
-	id_order VARCHAR(36) NOT NULL PRIMARY KEY,
-	id_client INT NOT NULL,
-	id_parcel INT NOT NULL,
-	total_value FLOAT NOT NULL,
-	order_address VARCHAR(256) NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS ordersTest (
+-- 	id_order VARCHAR(36) NOT NULL PRIMARY KEY,
+-- 	id_client INT NOT NULL,
+-- 	id_parcel INT NOT NULL,
+-- 	total_value FLOAT NOT NULL,
+-- 	order_address VARCHAR(256) NOT NULL
+-- );
